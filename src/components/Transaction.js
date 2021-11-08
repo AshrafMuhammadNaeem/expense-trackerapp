920d7b596d0ed0ccfe1fe66479cf0fed69dc6ebe
AshrafMuhammadNaeem <67267567+AshrafMuhammadNaeem@users.noreply.github.com>
2021-11-08 22:32:26 +0500

    Update Transaction.js
    based on ternary operator determine dynamically the amount as expenses(-) and income(+)

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,11 +1,12 @@
 import React from 'react'
 
 export const Transaction = ({transaction}) => {
+    const sign = transaction.ammount > 0 ? '+' : '-';
     
     
     return (
         <li className={transaction.amount < 0 ? 'red' : 'blue'}>
-           { transaction.text } <span>400${(transaction.amount)}</span><button className="delete-btn">remove</button>
+           { transaction.text } <span>{sign}${(transaction.amount)}</span><button className="delete-btn">remove</button>
         </li>
     )
 }
