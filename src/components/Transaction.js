7c16ab0bcea711ac7bafaaee70f9fa548fc0586c
AshrafMuhammadNaeem <67267567+AshrafMuhammadNaeem@users.noreply.github.com>
2021-11-08 22:28:31 +0500

    className based on ternary operator
    changed the color of box based on class, edited css file

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -2,8 +2,9 @@ import React from 'react'
 
 export const Transaction = ({transaction}) => {
     
+    
     return (
-        <li className={transaction.amount < 0 ? 'red' : 'green'}>
+        <li className={transaction.amount < 0 ? 'red' : 'blue'}>
            { transaction.text } <span>400${(transaction.amount)}</span><button className="delete-btn">remove</button>
         </li>
     )
