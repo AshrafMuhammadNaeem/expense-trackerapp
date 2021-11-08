f0424f5faf007cd6e12d6c8941cfdeb4876c9c73
AshrafMuhammadNaeem <67267567+AshrafMuhammadNaeem@users.noreply.github.com>
2021-11-08 15:40:43 +0500

    Update App.js
    Imported Globalprovider

--- src/App.js
+++ src/App.js
@@ -5,10 +5,11 @@ import { Balance } from './components/Balance';
 import { Header } from './components/Header';
 import { IncomeExpense } from './components/IncomeExpense';
 import { TransactionList } from './components/TransactionList';
+import { GlobalProvider } from './context/GlobalState';
 
 function App() {
   return (
-    <div> 
+    <GlobalProvider> 
       <Header />
 
     <div className="container">
@@ -19,7 +20,7 @@ function App() {
       <AddTransaction />
     </div>
 
-    </div>
+    </GlobalProvider>
     
   );
 }
