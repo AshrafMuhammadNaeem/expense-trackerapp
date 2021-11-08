48e3711a264dba02493b89fbb9aa7b2ae10bf703
AshrafMuhammadNaeem <67267567+AshrafMuhammadNaeem@users.noreply.github.com>
2021-11-08 15:02:01 +0500

    Create Balance.js

---
+++ src/components/Balance.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Balance = () => {
+    return (
+        <>
+            <h4>Total Balance</h4>
+            <h1 id="balance">$2000.00</h1>
+        </>
+    )
+}
