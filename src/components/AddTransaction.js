fbbbe0da9ea7371a48738cc18de4254767e9a8e4
AshrafMuhammadNaeem <67267567+AshrafMuhammadNaeem@users.noreply.github.com>
2021-11-08 15:28:41 +0500

    Update AddTransaction.js
    added onChange event to handle update transaction and amount

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -2,20 +2,22 @@ import React, {useState} from 'react'
 
 export const AddTransaction = () => {
     const [text, setText] = useState('');
-    const [aamount, setAmount]= useState(0);
+    const [amount, setAmount]= useState(0);
     return (
         <>
             <h3>Add new transaction</h3>
       <form>
         <div className="form-control">
           <label htmlFor="text">Text</label>
-          <input type="text"  placeholder="Enter transaction name..." />
+          <input type="text" value={text} onChange={(e) => setText(e.target.value)}
+           placeholder="Enter transaction name..." />
         </div>
         <div className="form-control">
           <label htmlFor="amount">Amount <br />
             (Income(+ve), Expenses(-ve) )</label
           >
-          <input type="number" placeholder="Enter amount..." />
+          <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} 
+           placeholder="Enter amount..." />
         </div>
         <button className="btn">Add transaction</button>
       </form>
