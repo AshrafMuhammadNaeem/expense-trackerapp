22d446613688608137d6465fba96bc449ec43624
AshrafMuhammadNaeem <67267567+AshrafMuhammadNaeem@users.noreply.github.com>
2021-11-11 11:44:00 +0500

    Update Balance.js
    imported globalcontet to get the transactions and update balance sheet

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, { useContext } from 'react'   // import useContext
+
+// import the GlobalContext
+import { GlobalContext } from '../context/GlobalState'
 
 export const Balance = () => {
+    const { transactions } = useContext(GlobalContext);
+
+
     return (
         <>
             <h4>Total Balance</h4>
