e82fb1ba8dca78b5abb03d7de2fc7aa179334892
AshrafMuhammadNaeem <67267567+AshrafMuhammadNaeem@users.noreply.github.com>
2021-11-11 15:17:22 +0500

    Update GlobalState.js
    get rid of dummy transactions.

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -4,12 +4,7 @@ import AppReducer from "./AppReducer";
 
 // Creating the Initial State
 const initialState = {
-    transactions: [
-        {id:1, text:'Books', amount: -700},
-        {id:2, text:'Salary', amount: 500000},
-        {id:1, text:'Investments', amount: -200000},
-        {id:1, text:'car', amount: -100000},
-    ]
+    transactions: [ ]
 }
 // Create Global Context using createContext
 export const GlobalContext = createContext(initialState);
