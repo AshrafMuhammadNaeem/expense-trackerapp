f5371d96d1c93271f939d4c48669baa3ee1dc7f8
AshrafMuhammadNaeem <67267567+AshrafMuhammadNaeem@users.noreply.github.com>
2021-11-11 14:25:44 +0500

    Update Transaction.js
    pulled in action delTransaction to Transaction.js  via useContext and added on click event on button to remove a ttransactio based on id.

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,14 +1,20 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import { GlobalContext } from '../context/GlobalState';
 
 export const Transaction = ({transaction}) => {
     // created ternary operator to get sign for expense and Income
+
+    const { delTransaction } = useContext(GlobalContext);
     const sign = transaction.amount < 0 ? '-' : '+'; 
+
+    
     
     
     return (
         // class based on ternary operator to get transaction amount changed color to red or blue based on expense/income
         <li className={transaction.amount < 0 ? 'red' : 'blue'}>
-           { transaction.text } <span>{sign}${Math.abs(transaction.amount)}</span><button className="delete-btn">remove</button>
+           { transaction.text } <span>{sign}${Math.abs(transaction.amount)}</span><button
+           onClick = {() => delTransaction(transaction.id)} className="delete-btn">remove</button>
         </li>
     )
 }
