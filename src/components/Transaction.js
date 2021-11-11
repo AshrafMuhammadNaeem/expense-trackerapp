f5f746f175c0e6ad98098c0e5ab869be3d45f740
AshrafMuhammadNaeem <67267567+AshrafMuhammadNaeem@users.noreply.github.com>
2021-11-11 14:35:24 +0500

    Update Transaction.js

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -5,6 +5,7 @@ export const Transaction = ({transaction}) => {
     // created ternary operator to get sign for expense and Income
 
     const { delTransaction } = useContext(GlobalContext);
+
     const sign = transaction.amount < 0 ? '-' : '+'; 
 
     
@@ -13,8 +14,8 @@ export const Transaction = ({transaction}) => {
     return (
         // class based on ternary operator to get transaction amount changed color to red or blue based on expense/income
         <li className={transaction.amount < 0 ? 'red' : 'blue'}>
-           { transaction.text } <span>{sign}${Math.abs(transaction.amount)}</span><button
-           onClick = {() => delTransaction(transaction.id)} className="delete-btn">remove</button>
+           { transaction.text } <span>{sign}${Math.abs(transaction.amount)}</span><button onClick={() => delTransaction(transaction.id)}
+            className="delete-btn">remove</button>
         </li>
     )
 }
