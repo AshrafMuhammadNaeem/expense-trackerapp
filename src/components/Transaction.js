b10e90b6921f0ea3365ccb23d614314a5831389d
AshrafMuhammadNaeem <67267567+AshrafMuhammadNaeem@users.noreply.github.com>
2021-11-11 11:22:22 +0500

    Update Transaction.js
    added comments

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
 export const Transaction = ({transaction}) => {
-    const sign = transaction.amount < 0 ? '-' : '+';
+    // created class based on ternary operator to get sign for expense and Income
+    const sign = transaction.amount < 0 ? '-' : '+'; 
     
     
     return (
+        // class based on ternary operator to get transaction amount changed color to red or blue based on expense/income
         <li className={transaction.amount < 0 ? 'red' : 'blue'}>
            { transaction.text } <span>{sign}${Math.abs(transaction.amount)}</span><button className="delete-btn">remove</button>
         </li>
