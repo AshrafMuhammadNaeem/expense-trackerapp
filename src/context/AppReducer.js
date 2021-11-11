e87ecfeddea919221d9b27fab6e398c196cfb184
AshrafMuhammadNaeem <67267567+AshrafMuhammadNaeem@users.noreply.github.com>
2021-11-11 14:43:54 +0500

    Update AppReducer.js

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,9 +1,15 @@
 export default (state, action) => {
     switch(action.type) {
-        case 'del-Transaction':
+        case "del-Transaction":
             return {
                 ...state,
                 transactions: state.transactions.filter(transaction => transaction.id  !== action.payload)
+            }
+
+        case "add-Transaction":
+            return {
+                ...state,
+                transactions: [action.payload, ...state.transactions]
             }
         default:
             return state;
