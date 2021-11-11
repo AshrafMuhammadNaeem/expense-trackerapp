fed3a9e1f80bc54ca594650f1566ebb4bf0b557e
AshrafMuhammadNaeem <67267567+AshrafMuhammadNaeem@users.noreply.github.com>
2021-11-11 13:54:48 +0500

    Update IncomeExpense.js
    importing useContext to get transactions and dynamically updating income and expenses

--- src/components/IncomeExpense.js
+++ src/components/IncomeExpense.js
@@ -1,17 +1,33 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { GlobalContext } from '../context/GlobalState'
 
 export const IncomeExpense = () => {
+    const { transactions } = useContext(GlobalContext);
+
+    const amounts = transactions.map(transaction => transaction.amount);
+
+  
+    const income = amounts
+      .filter(item => item > 0)
+      .reduce((sum, item) => (sum += item), 0)
+      .toFixed(2);
+  
+    const expense = (
+      amounts.filter(item => item < 0).reduce((sum, item) => (sum += item), 0) *
+      -1
+    ).toFixed(2);
+
     return (
         <div className="incExp-container">
             <div>
             <h4>Income </h4>
-            <p  className = "green"> +0.00$ </p>
+            <p  className = "green"> {income}$ </p>
 
             </div>
             
             <div>
                 <h4> Expenses </h4>
-                <p  className="red">-0.00$ </p>
+                <p  className="red">{expense}$ </p>
             </div>
         </div>
     )
